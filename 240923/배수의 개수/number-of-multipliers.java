6dfb146635a428ad0c449241f464171ce2838b7e
EunHee-Genius <yangeh0204@gmail.com>
2024-09-23 15:20:35 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240923/배수의 개수/number-of-multipliers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int three = 0;
+        int five = 0;
+        int[] arr = new int[10];
+
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+            if (arr[i]%3 ==0){
+                three++;
+            } else if(arr[i]%5 == 0){
+                five++;
+            }
+        }System.out.print(three + " " five);
+    
+    }
+}
