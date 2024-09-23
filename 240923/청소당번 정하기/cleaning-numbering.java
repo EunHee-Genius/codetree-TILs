2c0cd28d8c309c3a806ef3c8b466690a4b1660ba
EunHee-Genius <yangeh0204@gmail.com>
2024-09-23 15:38:25 +0900

    Title: 청소당번 정하기, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240923/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int kyo = 0;
+        int bok = 0;
+        int hwa = 0;
+
+        for(int i = 1; i < n; i++){
+            if(i%2==0 && i%3!=0 && i%12!=0){
+                kyo++;
+            }
+            if(i%3==0 && i%12!=0){
+                bok++;
+            }
+            if(i%12==0){
+                hwa++;
+            }
+        }
+        System.out.println(kyo + " " + bok + " " + hwa);
+    }
+}
