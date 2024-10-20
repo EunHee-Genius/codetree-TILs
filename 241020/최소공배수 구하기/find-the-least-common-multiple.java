d37a292133c312a076b0cca1519889ecfef84771
EunHee-Genius <yangeh0204@gmail.com>
2024-10-20 16:06:09 +0900

    Title: 최소공배수 구하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241020/최소공배수 구하기/find-the-least-common-multiple.java
+++ 241020/최소공배수 구하기/find-the-least-common-multiple.java
@@ -10,9 +10,9 @@ public class Main {
 
     public static void func(int n, int m){
         int x = 0;
-        for(int i=1; i<Math.min(n, m); i++){
+        for(int i=1; i<=Math.min(n,m); i++){
             if(n%i == 0 && m%i == 0){
-                    x=i;
+                x = i;
             }
         }
 
