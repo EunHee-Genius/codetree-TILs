3a7ae1946fda9e8885f1569688ba4dedada04e6f
EunHee-Genius <yangeh0204@gmail.com>
2025-02-11 15:10:32 +0900

    Title: 블럭쌓는 명령2, Time: 130ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250211/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int box = sc.nextInt();
+        int mr = sc.nextInt();
+        int[] arr = new int[box];
+        Arrays.fill(arr, 0);
+        int cnt = 0;
+
+        for(int i = 0; i < mr; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for(int j = a; j <= b; j++){
+                arr[j] += 1;
+            }
+        }
+
+        for(int i = 1; i <arr.length; i++){
+            if(arr[i-1] > arr[i]){
+                System.out.println(arr[i-1]);
+            }
+        }
+    }
+}
