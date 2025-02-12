834e2b98904d0d6d169b0b40aa3b6b4a4890d8c4
EunHee-Genius <yangeh0204@gmail.com>
2025-02-12 14:27:58 +0900

    Title: 왔다 갔던 구역 2, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250212/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();   // 명령 횟수
+        int[] line = new int[200];  // 이동칸 배열
+        Arrays.fill(line, 0);
+
+        int now = 100;
+        for(int i = 0; i < n; i++){
+//            line[start] += 1;
+            int go = sc.nextInt();
+            String dir = sc.next();
+            if(dir.equals("R")){
+//                System.out.println("11111111111111111");
+                int right = now + go;
+                for(int j = now; j < right; j++){
+                    line[j] += 1;
+                    now++;
+//                    System.out.println(now + " " + go);
+                }
+            }
+            else if(dir.equals("L")){
+//                int go = 100 - sc.nextInt();
+                int left = now - go;
+                for(int j = now; j > left; j--){
+                    line[j] += 1;
+                    now--;
+//                    System.out.println(now + " " + go);
+                }
+            }
+        }
+       int cnt = 0;
+        for(int i = 0; i < line.length; i++){
+            if(line[i] >= 2){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
