89b39359cbcbb0d07ec46519c4cc08b88cf96824
EunHee-Genius <yangeh0204@gmail.com>
2025-02-13 10:48:44 +0900

    Title: 흰검 칠하기, Time: 143ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250213/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt(); // 명령 횟수
+            int[] tile = new int[1000]; // 타일
+            int[] visited = new int[1000];  // 방문여부
+
+            // 시작은 100에서
+            int start = 500;
+
+            for(int i = 0; i < n; i++) {
+                int go = sc.nextInt();  // 얼마나 갈지
+                String dir = sc.next(); // 어느 방향
+
+                if(dir.equals("R")){
+                    for(int j = start; j < start+go; j++){
+                        tile[j] = 1;    // 검정색일땐 1로 만들어버리기
+                        visited[j]++;
+                        }
+                    start = start+go-1;
+//                    System.out.println("start : " + start);
+                }
+                else if(dir.equals("L")){
+                    for(int j = start; j > start-go; j--){
+                        tile[j] = 2;   // 하얀색일땐 2로 만들어버리기
+                        visited[j]++;
+                    }
+                    start = start - go + 1;
+//                    System.out.println("start : " + start);
+                }
+            }
+            int white = 0;
+            int black = 0;
+            int gray = 0;
+            for(int i = 0; i<tile.length; i++){
+                if(visited[i] >= 4){
+                    gray++;
+                }else{
+                    if(tile[i] == 1){
+                        black++;
+                    }else if(tile[i] == 2){
+                        white++;
+                    }
+                }
+            }
+            System.out.print(white + " " + black + " " + gray);
+    }
+}
