6d4adc2583fa4cef79dcd84356ad9876c98fa1b3
EunHee-Genius <yangeh0204@gmail.com>
2025-02-13 15:13:47 +0900

    Title: 사각형의 총 넓이 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 250213/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250213/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -11,10 +11,10 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i = 0; i < n; i++){
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
+            int x1 = sc.nextInt()+100;
+            int y1 = sc.nextInt()+100;
+            int x2 = sc.nextInt()+100;
+            int y2 = sc.nextInt()+100;
 
             for(int x = x1; x < x2; x++){
                 for(int y = y1; y < y2; y++){
