eaac4c9e3f78343011081d1bc93d6ec4821dc34f
EunHee-Genius <yangeh0204@gmail.com>
2025-06-23 21:25:29 +0900

    Title: 최고의 33위치, Time: 157ms, Memory: 32MB, Status: Passed - Codetree

--- 250623/최고의 33위치/best-place-of-33.java
+++ 250623/최고의 33위치/best-place-of-33.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 public class Main {
-
+    
+    // 0. 상수 선언
     public static final int MAX_N = 20;
     public static int[][] grid = new int[MAX_N][MAX_N];
 
-    // 금 갯수 세어보자
+    // 3. 금 갯수 세는 함수 만들기 (금 갯수 리턴)
     public static int func(int rowS, int colS, int rowE, int colE){
         int numOfGold = 0;
 
@@ -21,13 +22,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int maxGold = 0;
 
+        // 1. 횟수 입력받기
         int n = sc.nextInt();
-
+        // 2. 2차원 배열 입력받기
         for(int row = 0; row < n; row++){
             for(int col = 0; col < n; col++){
                 grid[row][col] = sc.nextInt();
             }
         }
+        // 4. 조건을 충족하는 배열을 싹 돌면서 maxGold를 선언하여 최대의 금 갯수를 저장한다.
     for(int row = 0; row < n; row++){
         for(int col = 0; col < n; col++){
             if(row + 2 >= n || col + 2 >= n){
@@ -38,6 +41,7 @@ public class Main {
             maxGold = Math.max(maxGold, numOfGold);
         }
     }
+    // 5. 그걸 뿌려준다
     System.out.println(maxGold);
     }
 }
