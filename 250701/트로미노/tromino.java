4dc2d92301b5591e8fc8007231f249079f605bfb
EunHee-Genius <yangeh0204@gmail.com>
2025-07-01 23:33:39 +0900

    Title: 트로미노, Time: 3100ms, Memory: 19MB, Status: Time Limit Exceed - Codetree

--- 250701/트로미노/tromino.java
+++ 250701/트로미노/tromino.java
@@ -31,11 +31,14 @@ class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+//        N = Integer.parseInt(st.nextToken());
+        N = sc.nextInt();
+//        M = Integer.parseInt(st.nextToken());
+        M = sc.nextInt();
 
         arr = new int[N][M];
         check = new boolean[N][M];
@@ -43,9 +46,10 @@ class Main {
         maxValue = 0;
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+//            st = new StringTokenizer(br.readLine());
             for (int j =0; j <M; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+//                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = sc.nextInt();
             }
         }
 
