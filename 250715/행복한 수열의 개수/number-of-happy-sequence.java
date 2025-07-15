60e93c99f582f9fd25d031f97309e2222579da6a
EunHee-Genius <yangeh0204@gmail.com>
2025-07-15 22:40:28 +0900

    Title: 행복한 수열의 개수, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250715/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+
+    public static final int MAX_N = 100;
+    public static final int MAX_M = 100;
+    public static int[][] grid = new int[MAX_N][MAX_N];
+    public static int[] seq = new int[MAX_N];
+    public static int n, m;
+
+    public static boolean func(){
+        int con = 1;
+        int maxcnt = 0;
+
+        for(int i = 1; i < n; i++){
+            if(seq[i-1] == seq[i]){
+                con++;
+            }else{
+                con = 1;
+            }
+            maxcnt = Math.max(maxcnt, con);
+        }
+        return maxcnt >= m;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < n; col++){
+                grid[row][col] = sc.nextInt();
+            }
+        }
+        int numHappy = 0;
+
+        for(int i =0; i <n; i++){
+            for(int j =0; j<n; j++){
+                seq[j] = grid[i][j];
+            }
+            if(func()){
+                numHappy++;
+            }
+        }
+        for(int i =0; i <n; i++){
+            for(int j =0; j<n; j++){
+                seq[j] = grid[j][i];
+            }
+            if(func()){
+                numHappy++;
+            }
+        }
+        System.out.print(numHappy);
+    }
+}
